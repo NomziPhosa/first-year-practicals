04b3f351279fead4b68370d48cc0580cd9d29491
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2025-07-16 09:36:01 +0200

    Practical 1
    Added the spec file and the code file for practical 1

---
+++ practical1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Hello, my name is Nomzi. Welcome to Code Brew."<<endl;
+    std::cout<<"How can I help you today?"<<endl;
+
+    return 0;
+}
